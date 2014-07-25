613421e40b5813bac5ad971e11920bfd4e5ce6bd
wtof <wtof1996@gmail.com>
2014-07-25 23:43:25 +0800

    Add the lua2bmp kernel project

---
+++ Bmp2Lua/Kernel/Lua2Bmp/io.hpp
@@ -0,0 +1,31 @@
+/*    Lua2Bmp Kernel V3
+ *
+ *    Copyright 2014 wtof1996
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *     this software is based in part on the work of the Independent JPEG Group.
+ *     this software is based in part on the libpng library.
+ *     this software is based in part on the Boost library.
+ */
+
+#ifndef IO_HPP_INCLUDED
+#define IO_HPP_INCLUDED
+
+namespace io
+{
+
+
+};//io
+
+
+#endif // IO_HPP_INCLUDED
